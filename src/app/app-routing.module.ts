235dcacdfa132e61d0d0892e565647ddd2f409a7
Gokul Das M <Gokul@NTP-LAP-723.chn.nestgroup.net>
2022-11-29 23:25:24 +0530

    quotes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PassengerDataComponent } from './passenger-data/passenger-data.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { QuotesComponent } from './quotes/quotes.component';
 import { TodosComponent } from './todos/todos.component';
 import { UsPublicComponent } from './us-public/us-public.component';
 import { UserDataComponent } from './user-data/user-data.component';
@@ -13,7 +14,8 @@ const routes: Routes = [
   {path:"userinfo",component:UserInfoComponent},
   {path:"productlist",component:ProductListComponent},
   {path:"userdata",component:UserDataComponent},
-  {path:"todos",component:TodosComponent}
+  {path:"todos",component:TodosComponent},
+  {path:"quotes",component:QuotesComponent}
 ];
 
 @NgModule({
