d697bb2e2424439f87e763e871c536c177514119
Gokul Das M <Gokul@NTP-LAP-723.chn.nestgroup.net>
2022-11-29 16:58:15 +0530

    product list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PassengerDataComponent } from './passenger-data/passenger-data.component';
+import { ProductListComponent } from './product-list/product-list.component';
 import { UsPublicComponent } from './us-public/us-public.component';
 import { UserInfoComponent } from './user-info/user-info.component';
 
 const routes: Routes = [
   {path:"",component:PassengerDataComponent},
   {path:"us",component:UsPublicComponent},
-  {path:"userinfo",component:UserInfoComponent}
+  {path:"userinfo",component:UserInfoComponent},
+  {path:"productlist",component:ProductListComponent}
 ];
 
 @NgModule({
