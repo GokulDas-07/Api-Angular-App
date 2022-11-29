f4d8d8ad48ca4b572d13a614851d776cad17a5ae
Gokul Das M <Gokul@NTP-LAP-723.chn.nestgroup.net>
2022-11-29 09:56:07 +0530

    passengerdata

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PassengerDataComponent } from './passenger-data/passenger-data.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:PassengerDataComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PassengerDataComponent } from './passenger-data/passenger-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PassengerDataComponent
   ],
   imports: [
     BrowserModule,
