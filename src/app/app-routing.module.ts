fd3dfc53f04920623a6513214068dba9427a9414
Gokul Das M <Gokul@NTP-LAP-723.chn.nestgroup.net>
2022-11-29 10:43:02 +0530

    userinfo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PassengerDataComponent } from './passenger-data/passenger-data.component';
 import { UsPublicComponent } from './us-public/us-public.component';
+import { UserInfoComponent } from './user-info/user-info.component';
 
 const routes: Routes = [
   {path:"",component:PassengerDataComponent},
-  {path:"us",component:UsPublicComponent}
+  {path:"us",component:UsPublicComponent},
+  {path:"userinfo",component:UserInfoComponent}
 ];
 
 @NgModule({
